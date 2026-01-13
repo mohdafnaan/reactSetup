c4b9cad6c9516d604f541327dbbbb9662f8d3d42
mohdafnaan <mohdafnaan833@gmail.com>
2026-01-13 20:37:36 +0530

    react setup

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+    return(
+        <>
+        <h1 className = "text-3xl font-bold text-center text-gray-800 mt-10">
+        hello babu
+        </h1>
+        <p>afnaan</p>
+        </>
+    )
+}
+export default App;
