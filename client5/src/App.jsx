d2e0bd9db563f4bf6985c1924e4a47baf37ea87a
mohdafnaan <mohdafnaan833@gmail.com>
2026-01-14 20:14:48 +0530

    react porfolio

---
+++ client5/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+    return(
+        <>
+        <h1 className="text-5xl text-center underline">my photo</h1>
+        <img className ="h-100 w-100
+      rounded-full mx-auto mt-10 blur- "  src="mount everest.jpg" alt="" />
+        </>
+    )
+}
+
+export default App;
